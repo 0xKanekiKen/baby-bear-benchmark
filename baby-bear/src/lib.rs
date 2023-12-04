154d8322ec75799fafe1c8c3a51a52931e78e95a
0xKanekiKen <100861945+0xKanekiKen@users.noreply.github.com>
2023-12-04 19:21:38 +0530

    feat: add neon implementation of packedbabybear from plonky3
    Signed-off-by: 0xKanekiKen <100861945+0xKanekiKen@users.noreply.github.com>

--- baby-bear/src/lib.rs
+++ baby-bear/src/lib.rs
@@ -6,3 +6,8 @@ mod baby_bear;
 mod extension;
 
 pub use baby_bear::*;
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+mod aarch64_neon;
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub use aarch64_neon::*;
