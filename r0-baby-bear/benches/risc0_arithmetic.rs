446ec1638670a0698c424de0d2e2a26321345396
0xKanekiKen <100861945+0xKanekiKen@users.noreply.github.com>
2023-12-05 10:20:57 +0530

    bench: add more benchs for risc0 babybear extension field
    Signed-off-by: 0xKanekiKen <100861945+0xKanekiKen@users.noreply.github.com>

--- r0-baby-bear/benches/risc0_arithmetic.rs
+++ r0-baby-bear/benches/risc0_arithmetic.rs
@@ -386,55 +386,373 @@ pub fn benchmark_montgomery(c: &mut Criterion)
 pub fn benchmark_canonical_ext(c: &mut Criterion)
 {
     let mut rng = rand::rngs::SmallRng::seed_from_u64(2);
-    c.bench_function("canonical_ext square", |b| {
+    c.bench_function("canonical_EXT square", |b| {
         let x: CanonicalExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).mul(black_box(x))))
     });
 
-    c.bench_function("canonical_ext inv", |b| {
+    c.bench_function("canonical_EXT inv", |b| {
         let x: CanonicalExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).inv()))
     });
 
-    c.bench_function("canonical_ext mul", |b| {
+    c.bench_function("canonical_EXT mul", |b| {
         let x: CanonicalExt = Elem::random(&mut rng);
         let y: CanonicalExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).mul(black_box(y))))
     });
 
-    c.bench_function("canonical_ext add", |b| {
+    c.bench_function("canonical_EXT add", |b| {
         let x: CanonicalExt = Elem::random(&mut rng);
         let y: CanonicalExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).add(black_box(y))))
     });
 
+    c.bench_function("canonical_EXT add-latency", |b| {
+        b.iter_batched(
+            || {
+                let mut vec: Vec<CanonicalExt> = Vec::new();
+                for _ in 0..10000 {
+                    vec.push(Elem::random(&mut rng));
+                }
+                vec
+            },
+            |x| x.iter().fold(CanonicalExt::default(), |x, y| x + *y),
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT add-throughput", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j): (CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt)| {
+                for _ in 0..1000 {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a + b,
+                        b + c,
+                        c + d,
+                        d + e,
+                        e + f,
+                        f + g,
+                        g + h,
+                        h + i,
+                        i + j,
+                        j + a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+    
+    c.bench_function("canonical_EXT sub-latency", |b| {
+        b.iter_batched(
+            || {
+                let mut vec: Vec<CanonicalExt> = Vec::new();
+                for _ in 0..10000 {
+                    vec.push(Elem::random(&mut rng));
+                }
+                vec
+            },
+            |x| x.iter().fold(CanonicalExt::default(), |x, y| x - *y),
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT sub-throughput", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j): (CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt)| {
+                for _ in 0..1000 {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a - b,
+                        b - c,
+                        c - d,
+                        d - e,
+                        e - f,
+                        f - g,
+                        g - h,
+                        h - i,
+                        i - j,
+                        j - a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT mul-throughput", |b| {
+        b.iter_batched(
+            || (Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),),
+            |(mut x, mut y, mut z, mut w) : (CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x * y, y * z, z * w, w * x);
+                }
+                (x, y, z, w)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT mul-latency", |b| {
+        b.iter_batched(
+            || Elem::random(&mut rng),
+            |mut x: CanonicalExt| {
+                for _ in 0..100 {
+                    x = x * x;
+                }
+                x
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT sqr-throughput>", |b| {
+        b.iter_batched(
+            || (Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),),
+            |(mut x, mut y, mut z, mut w) : (CanonicalExt, CanonicalExt, CanonicalExt, CanonicalExt)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x.clone() * x.clone(), y.clone() * y.clone(), z.clone() * z.clone(), w.clone() * w.clone());
+                }
+                (x, y, z, w)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("canonical_EXT sqr-latency", |b| {
+        b.iter_batched(
+            || Elem::random(&mut rng),
+            |mut x: CanonicalExt| {
+                for _ in 0..100 {
+                    x = x.clone() * x.clone();
+                }
+                x
+            },
+            BatchSize::SmallInput,
+        )
+    });
 }
 
-pub fn benchmark_montgomery_ext(c: &mut Criterion)
+fn benchmark_montgomery_ext(c: &mut Criterion)
 {
     let mut rng = rand::rngs::SmallRng::seed_from_u64(2);
-    c.bench_function("montgomery_ext square", |b| {
+    c.bench_function("montgomery_EXT square", |b| {
         let x: MontgomeryExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).mul(black_box(x))))
     });
 
-    c.bench_function("montgomery_ext inv", |b| {
+    c.bench_function("montgomery_EXT inv", |b| {
         let x: MontgomeryExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).inv()))
     });
 
-    c.bench_function("montgomery_ext mul", |b| {
+    c.bench_function("montgomery_EXT mul", |b| {
         let x: MontgomeryExt = Elem::random(&mut rng);
         let y: MontgomeryExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).mul(black_box(y))))
     });
 
-    c.bench_function("montgomery_ext add", |b| {
+    c.bench_function("montgomery_EXT add", |b| {
         let x: MontgomeryExt = Elem::random(&mut rng);
         let y: MontgomeryExt = Elem::random(&mut rng);
         b.iter(|| black_box(black_box(x).add(black_box(y))))
     });
 
+    c.bench_function("montgomery_EXT add-latency", |b| {
+        b.iter_batched(
+            || {
+                let mut vec: Vec<MontgomeryExt> = Vec::new();
+                for _ in 0..10000 {
+                    vec.push(Elem::random(&mut rng));
+                }
+                vec
+            },
+            |x| x.iter().fold(MontgomeryExt::default(), |x, y| x + *y),
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT add-throughput", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j): (MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt)| {
+                for _ in 0..1000 {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a + b,
+                        b + c,
+                        c + d,
+                        d + e,
+                        e + f,
+                        f + g,
+                        g + h,
+                        h + i,
+                        i + j,
+                        j + a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT sub-latency", |b| {
+        b.iter_batched(
+            || {
+                let mut vec: Vec<MontgomeryExt> = Vec::new();
+                for _ in 0..10000 {
+                    vec.push(Elem::random(&mut rng));
+                }
+                vec
+            },
+            |x| x.iter().fold(MontgomeryExt::default(), |x, y| x - *y),
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT sub-throughput", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                    Elem::random(&mut rng),
+                )
+            },
+            |(mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j): (MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt)| {
+                for _ in 0..1000 {
+                    (a, b, c, d, e, f, g, h, i, j) = (
+                        a - b,
+                        b - c,
+                        c - d,
+                        d - e,
+                        e - f,
+                        f - g,
+                        g - h,
+                        h - i,
+                        i - j,
+                        j - a,
+                    );
+                }
+                (a, b, c, d, e, f, g, h, i, j)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT mul-throughput", |b| {
+        b.iter_batched(
+            || (Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),),
+            |(mut x, mut y, mut z, mut w) : (MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x * y, y * z, z * w, w * x);
+                }
+                (x, y, z, w)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT mul-latency", |b| {
+        b.iter_batched(
+            || Elem::random(&mut rng),
+            |mut x: MontgomeryExt| {
+                for _ in 0..100 {
+                    x = x * x;
+                }
+                x
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT sqr-throughput>", |b| {
+        b.iter_batched(
+            || (Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),
+            Elem::random(&mut rng),),
+            |(mut x, mut y, mut z, mut w) : (MontgomeryExt, MontgomeryExt, MontgomeryExt, MontgomeryExt)| {
+                for _ in 0..25 {
+                    (x, y, z, w) = (x.clone() * x.clone(), y.clone() * y.clone(), z.clone() * z.clone(), w.clone() * w.clone());
+                }
+                (x, y, z, w)
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("montgomery_EXT sqr-latency", |b| {
+        b.iter_batched(
+            || Elem::random(&mut rng),
+            |mut x: MontgomeryExt| {
+                for _ in 0..100 {
+                    x = x.clone() * x.clone();
+                }
+                x
+            },
+            BatchSize::SmallInput,
+        )
+    });
 }
 
 fn bench_babybear(c: &mut Criterion) {
